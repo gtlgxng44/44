e6c9997e2d7544b3577cc376af51c4127b945705
gtlgxng44 <Bytebwoy@gmail.com>
2025-11-21 17:13:17 +0300

    Add Firebase configuration file

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// Replace values with your Firebase config if you use Firebase later
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
